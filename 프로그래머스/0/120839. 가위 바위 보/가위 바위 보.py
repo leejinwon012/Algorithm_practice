7c58379f4bdeacbf26cdc220f64099311baa1353
Jinwon <78424970+leejinwon012@users.noreply.github.com>
2024-03-18 17:31:02 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,12 +1,12 @@
 def solution(rsp):
-    answer = []
+    answer = []         # " " 문자열엔 append 사용이 불가능해서 answer을 리스트로
     for i in rsp:
         if i in "2":
-            answer.append('0')
+            answer.append('0') 
         elif i in "0":
             answer.append('5')
         elif i in "5":
             answer.append('2')
 
-    answer = "".join(answer)
+    answer = "".join(answer)        # join 함수 개념 확실히!
     return answer
