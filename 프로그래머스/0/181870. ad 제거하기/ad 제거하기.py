76fcbf97fcc5656d62e708bb2c33e75540eedf88
DoriS2 <78424970+leejinwon012@users.noreply.github.com>
2024-03-22 02:21:53 +0900

    [level 0] Title: ad 제거하기, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -1,6 +1,6 @@
 def solution(strArr):
     result = []
     for i in strArr:
-        if 'ad' not in i:
+        if 'ad' not in i:   # 문자열 반복동안 해당 리스트 수정은 안하는게 좋음
             result.append(i)
     return result
