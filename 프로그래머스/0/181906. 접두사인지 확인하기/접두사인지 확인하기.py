f9e73d62e59e72904d56ad0423a03e7379737e45
DoriS2 <78424970+leejinwon012@users.noreply.github.com>
2024-03-28 20:47:52 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,9 +1,5 @@
 def solution(my_string, is_prefix):
-    if len(my_string) < len(is_prefix):  # 문자열이 접두사보다 짧으면 접두사가 될 수 없음
+    if my_string.startswith(is_prefix):
+        return 1
+    else:
         return 0
-
-    for i in range(len(is_prefix)):
-        if my_string[i] != is_prefix[i]:  # 문자열이 일치하지 않는 경우
-            return 0
-
-    return 1  # 문자열이 모두 일치하는 경우
