8dfd77aec09065a31ee1305c4cb5bc9292ea1bbf
DoriS2 <78424970+leejinwon012@users.noreply.github.com>
2024-04-11 02:14:32 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 1
+    factorial = 1
+
+    while factorial <= n:
+        answer += 1
+        factorial = factorial * answer
+    answer = answer - 1
+
+    return answer
