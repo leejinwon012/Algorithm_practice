b036453285b3c3bc3c847cf393ab05870bf4d340
DoriS2 <78424970+leejinwon012@users.noreply.github.com>
2024-05-10 10:20:11 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,11 @@
+def solution(s):
+    s = s.split()
+    stack = []
+
+    for i in s:
+        if i == 'Z':
+            stack.pop()
+        else:
+            stack.append(int(i))
+
+    return sum(stack)
